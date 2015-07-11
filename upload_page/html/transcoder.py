716fec85d743386fe10fa98decd3838560f884e3
root <you@example.com>
2015-07-11 17:12:47 +0800

    upload_page 添加config功能

--- upload_page/html/transcoder.py
+++ upload_page/html/transcoder.py
@@ -8,15 +8,9 @@ import re
 class Transcoder:
     def handle_body(self, name, body):
         if name == b'channel_id':
-            m = hashlib.md5()
-            m.update(body)
-            md5_filename_str = m.hexdigest()
-
-            target_filename = self.target_filename(md5_filename_str)
-            f = open(target_filename, mode='ab')
-            self.filehandle = f
-        else:
-            self.filehandle.write(body)
+            self.init_write_handler(body)
+        elif(name == b'upload'):
+            self.write_handler.write(body)
 
     def read_conf(self):
         configfile = 'conf/transcoder.conf'
@@ -32,14 +26,19 @@ class Transcoder:
         config = self.read_conf()
         basedir = config['storage']['dir']
 
-        regex_file = re.search('(\w{2})(\w{2})', filename)
-        if regex_file:
-            dir1 = regex_file.group(1)
-            dir2 = regex_file.group(2)
-
+        dir1 = filename[:2]
+        dir2 = filename[2:4]
         target_dir = basedir + '/' + dir1 + '/' + dir2
         if not os.path.exists(target_dir):
             os.makedirs(target_dir)
 
         target_filename = target_dir + '/' + filename
         return target_filename
+
+    def init_write_handler(self, channel_id ):
+        m = hashlib.md5()
+        m.update(channel_id)
+        md5_filename_str = m.hexdigest()
+        target_filename = self.target_filename(md5_filename_str)
+        f = open(target_filename, mode='ab')
+        self.write_handler = f
