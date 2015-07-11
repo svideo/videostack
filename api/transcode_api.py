220277cf4a0814393edf2fef6b022bb0fa7083ca
laodifang <ithink.ren@gmail.com>
2015-07-11 18:16:41 +0800

    mod transcode_api.py

--- api/transcode_api.py
+++ api/transcode_api.py
@@ -1,17 +1,23 @@
 from flask import Flask
+from flask import make_response
 import redis
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "x100speed_transcode"
 
 @app.route("/getvideomaxid")
 def getvideomaxid():
     r      = redis.StrictRedis(host='localhost', port=6379, db=0, password='foobared')
     max_id = r.incr("video_id_max")
-    return str(max_id)
+    resp   = make_response( str(max_id) )
+    
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    
+    return resp
 
 if __name__ == "__main__":
     app.debug = True
