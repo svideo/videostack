5053e288a492c82f3f8c1354b359c384767aa9f9
laodifang <ithink.ren@gmail.com>
2015-07-13 09:03:26 -0400

    x100speed admin system

--- install/redis/README.md
+++ install/redis/README.md
@@ -1,13 +1,13 @@
 x100speed_transcode Redis 数据结构说明文档
 =======================================
 
-### video_id_max
+### video\_id\_max
 #### 描述
     存储当前最大的视频id值，利用redis原子性INCR获取，数据类型为数字的字符串，初始值为1000000
 #### 数据结构
 | Key            | Value         | Type   |
 | -------------- |:-------------:| ------:|
-| video_id_max   | 100000        | String |
+| video\_id\_max | 100000        | String |
 
 #### 操作
     初始化
@@ -16,3 +16,17 @@ x100speed_transcode Redis 数据结构说明文档
     增加Value
     INCR video_id_max
 
+### video\_staff\_hash
+#### 描述
+    使用hash结构存储staff转码机ip地址、load。ip数据类型为字符串，load数据类型为浮点数以字符串形式保存
+| Data Structure | Key                | Field         | Type   | Value   | Type   |
+| -------------- |:------------------:|:-------------:|:------:|:-------:| ------:|
+| hashes         | video\_staff\_hash | 192.168.1.100 | String | 90.5    | String |
+
+#### 操作
+    添加staff主机
+    HSET staff_hash 192.168.1.100 0
+    
+    更新staff主机load
+    HSET staff_hash 192.168.1.100 99.9
+    
---
+++ x100speed/admin/admin.py
@@ -0,0 +1,51 @@
+﻿from flask import Flask, request
+from flask import make_response
+import redis
+from pprint import pprint
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Welcome X100speed_transcode Admin System'
+
+@app.route('/x100speedtranscode/videostaff/add')
+@app.route('/x100speedtranscode/videostaff/updata')
+def video_staff_add():
+    ip = request.args.get('ip')
+    if(not ip):
+        return "{'status':'failed', 'message':'ip is null'}"
+    
+    load = request.args.get('load')
+    if(not load):
+        return "{'status':'failed', 'message':'load is null'}"
+
+    r = redis_connect()
+    r.hset("video_staff_hash", ip, load)
+    
+    return "{'status':'success', 'message':''}"
+
+@app.route('/x100speedtranscode/videostaff/delete')
+def video_staff_delete():
+    ip = request.args.get('ip')
+    if(not ip):
+        return "{'status':'failed', 'message':'ip is null'}"
+    
+    r = redis_connect()
+    r.hset("video_staff_hash", ip)
+
+    return "{'status':'success', 'message':''}"
+
+def redis_connect():
+    host='127.0.0.1'
+    port=6379
+    db=0
+    password='foobared'
+
+    r = redis.StrictRedis(host = host, port = port, password = password, db = db)
+    return r
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
