0d054e1c9200389e6be4e8ccdc3d85a9451d54e7
laodifang <ithink.ren@gmail.com>
2015-07-19 01:26:40 +0800

    add m3u8 player

--- interface/x100speed_interface.py
+++ interface/x100speed_interface.py
@@ -16,12 +16,8 @@ def video_uuid_get():
     hash_string = ip + userAgent + str(millisecond)
     idgen       = x100idgen.IdGen()
     uuid        = idgen.gen_id(hash_string)
-    ip          = "10.221.193.196"
+    ip          = "www.x100speed.com"
 
-    #r     = redis_connect()
-    #value = '|' + '|' + ip + '|' 
-    #ret   = r.hset("x100speed_hash_uuid", uuid, value)
-    
     infor = '{"uuid":"' + uuid + '","ip":"' + ip + '"}'
     response.data = infor
     return response
@@ -200,7 +196,6 @@ def video_uuid_new_image_get():
     response.data = '{"status":"success", "message":"", "image_url":"' + image_url + '"}'
     return response
 
-
 @app.route("/interface/video_uuid_segment_add")
 def video_uuid_segment_add():
     uuid         = request.args.get('uuid')
@@ -222,7 +217,7 @@ def video_uuid_segment_add():
         response.data = '{"status":"failed", "message":"params have error"}'
         return response
     
-    sorted_set_key    = 'x100speed_sortedset_uuid_' + bitrate
+    sorted_set_key    = 'x100speed_sortedset_' + uuid + '_' + bitrate
     sorted_set_score  = create_time
     sorted_set_member = fragment_id + '|' + hostname + '|' + storage_path + '|' + create_time \
                         + '|' + fps + '|' + frame_count + '|' + file_size
@@ -232,6 +227,106 @@ def video_uuid_segment_add():
     response.data = '{"status":"success", "message":""}'
     return response
 
+@app.route("/interface/<play_url>.m3u8")
+def video_uuid_play(play_url):
+    uuid = play_url
+
+    response = make_response()
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Content-Type'] = 'application/vnd.apple.mpegurl'
+
+    r   = redis_connect()
+    ret = r.hget('x100speed_hash_uuid', uuid)
+    if not ret:
+        response.data = ""
+        return response
+
+    uuid_info      = ret.decode()
+    uuid_info_list = uuid_info.split('|')
+    play_type      = "live"
+    if uuid_info_list[0] == "success":
+        play_type = "vod"
+    elif uuid_info_list[0] == "failed":
+        response.data = ""
+        return response
+
+    ip            = uuid_info_list[2]
+    bitrates      = uuid_info_list[3]
+    bitrates_list = bitrates.split(',')
+    
+    m3u8_value = '#EXTM3U\n'
+    for bitrate in bitrates_list:
+        total_bitrate = int(bitrate) * 1024
+        m3u8_value   += '#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=' + str(total_bitrate) + '\n'
+        #m3u8_value   += 'http://' + ip + '/' + uuid + '_' + bitrate + '.m3u8\n'
+        m3u8_value   += 'http://10.221.193.196:5000/interface/' + uuid + '_' + bitrate + '.m3u8\n'
+
+    response.data = m3u8_value
+    return response
+
+@app.route("/interface/<play_url>_<int:play_bitrate>.m3u8")
+def video_uuid_play_child(play_url, play_bitrate):
+    uuid    = play_url
+    bitrate = play_bitrate
+
+    response = make_response()
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Content-Type'] = 'application/vnd.apple.mpegurl'
+
+    r   = redis_connect()
+    ret = r.hget('x100speed_hash_uuid', uuid)
+    if not ret:
+        response.data = ""
+        return response
+
+    uuid_info      = ret.decode()
+    uuid_info_list = uuid_info.split('|')
+    play_type      = "live"
+    if uuid_info_list[0] == "success":
+        play_type = "vod"
+    elif uuid_info_list[0] == "failed":
+        response.data = ""
+        return response
+
+    uuid_sortedset = 'x100speed_sortedset_' + uuid + '_' + str(bitrate)
+    fragment_list  = r.zrange(uuid_sortedset, -3, -1)
+    if not fragment_list:
+        response.data = ""
+        return response
+
+    uuid_m3u8             = ""
+    uuid_m3u8_tmp         = ""
+    uuid_m3u8_sequence    = ""
+    max_fragment_duration = 0.000
+    
+    for fragment in fragment_list:
+        fragment_string = fragment.decode()
+        fragment_array  = fragment_string.split('|')
+        
+        if not uuid_m3u8_sequence:
+            uuid_m3u8_sequence = str(fragment_array[0])
+ 
+        fragment_duration = round(int(fragment_array[5]) / int(fragment_array[4]), 3)
+        if fragment_duration > max_fragment_duration:
+            max_fragment_duration = fragment_duration
+
+        fragment_url  = fragment_array[1] + fragment_array[2]
+        uuid_m3u8_tmp += '#EXTINF:' + str(fragment_duration)
+        uuid_m3u8_tmp += "\n"
+        uuid_m3u8_tmp += fragment_url
+        uuid_m3u8_tmp += "\n"
+    
+    uuid_m3u8 += "#EXTM3U\n"
+    uuid_m3u8 += "#EXT-X-VERSION:5\n"
+    uuid_m3u8 += "#EXT-X-TARGETDURATION:" + str(max_fragment_duration) + "\n"
+    uuid_m3u8 += "#EXT-X-MEDIA-SEQUENCE:" + uuid_m3u8_sequence + "\n"
+    uuid_m3u8 += "#EXT-X-ALLOW-CACHE:YES\n"
+    uuid_m3u8 += uuid_m3u8_tmp
+
+    response.data = uuid_m3u8
+    return response
 
 def redis_connect():
     host='127.0.0.1'
@@ -244,4 +339,4 @@ def redis_connect():
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
